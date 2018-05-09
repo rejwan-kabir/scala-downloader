923d24fb077a1ef13d015c24444816a5066eca0f
Rejwan Shamsul Kabir <rejwan.shuvo@gmail.com>
2018-05-09 16:46:47 +0600

    download works with split files

--- src/main/scala/agoda/Downloader.scala
+++ src/main/scala/agoda/Downloader.scala
@@ -1,7 +1,10 @@
 package agoda
 
-import java.io.{BufferedOutputStream, File, FileOutputStream}
+import java.io._
 import java.net.{HttpURLConnection, URL}
+import java.nio.channels.FileChannel
+import java.nio.file.StandardOpenOption._
+import java.nio.file.{Files, Paths}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
@@ -9,8 +12,10 @@ import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success}
 
 object Downloader extends App {
-  type Destination = String
-  implicit val destination: Destination = "/home/rejwan/TEMP/"
+  type FilePath = String
+  implicit val destination: FilePath = "/home/rejwan/TEMP/"
+  val tempDir = destination // System.getProperty("java.io.tmpdir", "/tmp")
+  val readChunk = 16 * 1024
 
   val urls = List(
     "http://longwallpapers.com/Desktop-Wallpaper/rain-wallpapers-hd-For-Desktop-Wallpaper.jpg",
@@ -21,7 +26,15 @@ object Downloader extends App {
     "https://i.pinimg.com/564x/3c/64/db/3c64db15ff4a2351cf29634eb7c9240c.jpg",
     "https://i.pinimg.com/564x/70/6c/bd/706cbd9f15223e48168941f89aefff22.jpg")
 
-  def fetch(urlString: String)(implicit destination: Destination): File = {
+  def fromInputStream(in: InputStream, bufferSize: Int): Stream[Array[Byte]] = {
+    val buffer = new Array[Byte](bufferSize)
+    in.read(buffer) match {
+      case -1 => Stream.empty
+      case len => buffer.slice(0, len) #:: fromInputStream(in, bufferSize)
+    }
+  }
+
+  def fetch(urlString: String)(implicit destination: FilePath): File = {
     println("Processing : " + urlString)
     val fileName = urlString.substring(urlString.lastIndexOf('/') + 1)
     val file = new File(destination + fileName)
@@ -29,11 +42,23 @@ object Downloader extends App {
     connection.setRequestMethod("GET")
     connection.addRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)")
     val in = connection.getInputStream
-    val out = new BufferedOutputStream(new FileOutputStream(file))
+    val out = FileChannel.open(Paths.get(destination + fileName), CREATE, WRITE)
     try {
-      val byteArray = Stream.continually(in.read).takeWhile(_ != -1).map(_.toByte).toArray
-      out.write(byteArray)
-      out.flush()
+      val stream = fromInputStream(in, readChunk)
+      stream.zipWithIndex.map {
+        case (data: Array[Byte], i: Int) =>
+          val file = new File(tempDir + fileName + "_" + i)
+          val intermediate = new BufferedOutputStream(new FileOutputStream(file))
+          intermediate.write(data)
+          intermediate.flush()
+          intermediate.close()
+          tempDir + fileName + "_" + i
+      }.foreach(filePath => {
+        val in = FileChannel.open(Paths.get(filePath), READ)
+        in.transferTo(0, in.size, out)
+        in.close()
+        Files.delete(Paths.get(filePath))
+      })
     } finally {
       out.close()
       connection.disconnect()
