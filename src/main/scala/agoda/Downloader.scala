31d5dd2e713872be9f2d2e7996888002930df859
Rejwan Shamsul Kabir <rejwan.shuvo@gmail.com>
2018-05-09 17:12:50 +0600

    command line setup done

--- src/main/scala/agoda/Downloader.scala
+++ src/main/scala/agoda/Downloader.scala
@@ -11,11 +11,14 @@ import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success}
 
+// run with command line argument:  http://longwallpapers.com/Desktop-Wallpaper/rain-wallpapers-hd-For-Desktop-Wallpaper.jpg,https://wallpaper.wiki/wp-content/uploads/2017/06/Light-water-close-up-nature-rain-wallpapers-HD.jpg,https://i.pinimg.com/564x/2e/88/31/2e8831e90095c14437bbb866dd7cd3ec.jpg,https://i.pinimg.com/564x/5c/46/e4/5c46e4d74edf8e4c396beda8a126397f.jpg,https://i.pinimg.com/564x/30/f9/51/30f9518869ddedf7bddd5e5a5e65d5a2.jpg,https://i.pinimg.com/564x/3c/64/db/3c64db15ff4a2351cf29634eb7c9240c.jpg,https://i.pinimg.com/564x/70/6c/bd/706cbd9f15223e48168941f89aefff22.jpg /home/rejwan/TEMP/
 object Downloader extends App {
   type FilePath = String
-  implicit val destination: FilePath = "/home/rejwan/TEMP/"
+  implicit val destination: FilePath = args(1)
   val readChunk = 16 * 1024
+  val urls = args(0).split(',').toList
 
+  /*
   val urls = List(
     "http://longwallpapers.com/Desktop-Wallpaper/rain-wallpapers-hd-For-Desktop-Wallpaper.jpg",
     "https://wallpaper.wiki/wp-content/uploads/2017/06/Light-water-close-up-nature-rain-wallpapers-HD.jpg",
@@ -24,6 +27,7 @@ object Downloader extends App {
     "https://i.pinimg.com/564x/30/f9/51/30f9518869ddedf7bddd5e5a5e65d5a2.jpg",
     "https://i.pinimg.com/564x/3c/64/db/3c64db15ff4a2351cf29634eb7c9240c.jpg",
     "https://i.pinimg.com/564x/70/6c/bd/706cbd9f15223e48168941f89aefff22.jpg")
+  */
 
   def fromInputStream(in: InputStream, bufferSize: Int): Stream[Array[Byte]] = {
     val buffer = new Array[Byte](bufferSize)
