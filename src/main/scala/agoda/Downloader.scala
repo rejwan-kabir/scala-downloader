61d4f4d27793cb75f859241fe91637cea366dd11
Rejwan Shamsul Kabir <rejwan.shuvo@gmail.com>
2018-05-09 17:58:52 +0600

    need to stop future sequence from propagating error

--- src/main/scala/agoda/Downloader.scala
+++ src/main/scala/agoda/Downloader.scala
@@ -16,18 +16,21 @@ object Downloader extends App {
   type FilePath = String
   implicit val destination: FilePath = args(1)
   val readChunk = 16 * 1024
-  val urls = args(0).split(',').toList
+  //val urls = args(0).split(',').toList
 
-  /*
   val urls = List(
+    //"https://i.pinimg.com/564x/5c/46/e4/shuvo.jpg",
     "http://longwallpapers.com/Desktop-Wallpaper/rain-wallpapers-hd-For-Desktop-Wallpaper.jpg",
     "https://wallpaper.wiki/wp-content/uploads/2017/06/Light-water-close-up-nature-rain-wallpapers-HD.jpg",
     "https://i.pinimg.com/564x/2e/88/31/2e8831e90095c14437bbb866dd7cd3ec.jpg",
     "https://i.pinimg.com/564x/5c/46/e4/5c46e4d74edf8e4c396beda8a126397f.jpg",
+    //"https://i.pinimg.com/564x/5c/46/e4/imon.jpg",
     "https://i.pinimg.com/564x/30/f9/51/30f9518869ddedf7bddd5e5a5e65d5a2.jpg",
     "https://i.pinimg.com/564x/3c/64/db/3c64db15ff4a2351cf29634eb7c9240c.jpg",
-    "https://i.pinimg.com/564x/70/6c/bd/706cbd9f15223e48168941f89aefff22.jpg")
-  */
+    "https://i.pinimg.com/564x/70/6c/bd/706cbd9f15223e48168941f89aefff22.jpg",
+    //"https://i.pinimg.com/564x/5c/46/e4/arshi.jpg"
+  )
+
 
   def fromInputStream(in: InputStream, bufferSize: Int): Stream[Array[Byte]] = {
     val buffer = new Array[Byte](bufferSize)
@@ -72,12 +75,12 @@ object Downloader extends App {
   val futureList: List[Future[File]] = urls.map(u => Future(fetch(u)))
   futureList.foreach(_.onComplete {
     case Success(file) => println(file.getAbsolutePath + " completed")
-    case Failure(ex) => println("exception on download in " + ex.getMessage)
+    case Failure(ex) => println("Exception on download in " + ex.getMessage)
   })
 
-  val futureSeq = Future.sequence(futureList)
+  val futureSeq = Future.sequence(futureList.map(f => f.map(Some(_)))) // https://users.scala-lang.org/t/converting-list-future-int-to-future-list-int-with-failed-futures/1765/3
   futureSeq.onComplete {
-    case Success(_) => println("All downloads completed")
+    case Success(x) => println("All downloads completed")
     case Failure(e) => println("Exception happened in " + e.getMessage)
   }
 
