3dd42decb1b024846fba857df04c47f076b954ae
Rejwan Shamsul Kabir <rejwan.shuvo@gmail.com>
2018-05-10 14:17:53 +0600

    delete works for http ftp

--- src/main/scala/agoda/Downloader.scala
+++ src/main/scala/agoda/Downloader.scala
@@ -3,8 +3,8 @@ package agoda
 import java.io._
 import java.net.{HttpURLConnection, URL, URLConnection}
 import java.nio.channels.FileChannel
-import java.nio.file.Paths
 import java.nio.file.StandardOpenOption._
+import java.nio.file.{Files, Paths}
 
 import sun.net.www.protocol.ftp.FtpURLConnection
 
@@ -13,7 +13,7 @@ import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success, Try}
 
-// run with command line argument: https://i.pinimg.com/564x/5c/46/e4/shuvo.jpg,http://longwallpapers.com/Desktop-Wallpaper/rain-wallpapers-hd-For-Desktop-Wallpaper.jpg,https://wallpaper.wiki/wp-content/uploads/2017/06/Light-water-close-up-nature-rain-wallpapers-HD.jpg,https://i.pinimg.com/564x/2e/88/31/2e8831e90095c14437bbb866dd7cd3ec.jpg,https://i.pinimg.com/564x/5c/46/e4/5c46e4d74edf8e4c396beda8a126397f.jpg,https://i.pinimg.com/564x/5c/46/e4/imon.jpg,https://i.pinimg.com/564x/30/f9/51/30f9518869ddedf7bddd5e5a5e65d5a2.jpg,https://i.pinimg.com/564x/3c/64/db/3c64db15ff4a2351cf29634eb7c9240c.jpg,https://i.pinimg.com/564x/70/6c/bd/706cbd9f15223e48168941f89aefff22.jpg,https://i.pinimg.com/564x/5c/46/e4/arshi.jpg
+// run with command line argument: https://i.pinimg.com/564x/5c/46/e4/shuvo.jpg,ftp://speedtest.tele2.net/512KB.zip,http://longwallpapers.com/Desktop-Wallpaper/rain-wallpapers-hd-For-Desktop-Wallpaper.jpg,ftp://speedtest.tele2.net/2MB.zip,https://wallpaper.wiki/wp-content/uploads/2017/06/Light-water-close-up-nature-rain-wallpapers-HD.jpg,https://i.pinimg.com/564x/2e/88/31/2e8831e90095c14437bbb866dd7cd3ec.jpg,ftp://speedtest.tele2.net/3MB.zip,https://i.pinimg.com/564x/5c/46/e4/5c46e4d74edf8e4c396beda8a126397f.jpg,https://i.pinimg.com/564x/5c/46/e4/imon.jpg,ftp://speedtest.tele2.net/5MB.zip,https://i.pinimg.com/564x/30/f9/51/30f9518869ddedf7bddd5e5a5e65d5a2.jpg,https://i.pinimg.com/564x/3c/64/db/3c64db15ff4a2351cf29634eb7c9240c.jpg,https://i.pinimg.com/564x/70/6c/bd/706cbd9f15223e48168941f89aefff22.jpg,https://i.pinimg.com/564x/5c/46/e4/arshi.jpg /home/rejwan/TEMP/
 object Downloader extends App {
   type FilePath = String
   implicit val destination: FilePath = if (args(1).endsWith("/")) args(1) else args + "/"
@@ -23,11 +23,15 @@ object Downloader extends App {
   /*
   val urls = List(
     "https://i.pinimg.com/564x/5c/46/e4/shuvo.jpg",
+    "ftp://speedtest.tele2.net/512KB.zip",
     "http://longwallpapers.com/Desktop-Wallpaper/rain-wallpapers-hd-For-Desktop-Wallpaper.jpg",
+    "ftp://speedtest.tele2.net/2MB.zip",
     "https://wallpaper.wiki/wp-content/uploads/2017/06/Light-water-close-up-nature-rain-wallpapers-HD.jpg",
     "https://i.pinimg.com/564x/2e/88/31/2e8831e90095c14437bbb866dd7cd3ec.jpg",
+    "ftp://speedtest.tele2.net/3MB.zip",
     "https://i.pinimg.com/564x/5c/46/e4/5c46e4d74edf8e4c396beda8a126397f.jpg",
     "https://i.pinimg.com/564x/5c/46/e4/imon.jpg",
+    "ftp://speedtest.tele2.net/5MB.zip",
     "https://i.pinimg.com/564x/30/f9/51/30f9518869ddedf7bddd5e5a5e65d5a2.jpg",
     "https://i.pinimg.com/564x/3c/64/db/3c64db15ff4a2351cf29634eb7c9240c.jpg",
     "https://i.pinimg.com/564x/70/6c/bd/706cbd9f15223e48168941f89aefff22.jpg",
@@ -42,7 +46,7 @@ object Downloader extends App {
     }
   }
 
-  def fetch(connection: URLConnection)(urlString: String, cleanUp: => Unit = ())(implicit destination: FilePath): File = {
+  private[this] def fetch(connection: URLConnection)(urlString: String, cleanUp: => Unit = ())(implicit destination: FilePath): File = {
     println("Processing : " + urlString)
     val fileName = urlString.substring(urlString.lastIndexOf('/') + 1)
     val file = new File(destination + fileName)
@@ -50,7 +54,7 @@ object Downloader extends App {
     val out = FileChannel.open(Paths.get(destination + fileName), CREATE, WRITE)
     try {
       val stream = fromInputStream(in, readChunk)
-      val splitFileStream = stream.zipWithIndex.map {
+      val splitFileList = stream.zipWithIndex.map {
         case (data: Array[Byte], i: Int) =>
           val file = File.createTempFile(fileName, s"_$i")
           val intermediate = new BufferedOutputStream(new FileOutputStream(file))
@@ -59,36 +63,50 @@ object Downloader extends App {
           intermediate.close()
           file.deleteOnExit()
           file.getAbsolutePath
-      }
-      splitFileStream.foreach(filePath => {
+      }.toList
+      splitFileList.foreach(filePath => {
         val in = FileChannel.open(Paths.get(filePath), READ)
         in.transferTo(0, in.size, out)
         in.close()
       })
-    } finally {
-      out.close()
+    } catch {
+      case ex: Exception =>
+        out.close()
+        Files.delete(Paths.get(destination + fileName))
+        throw ex
+    }
+    finally {
+      if (out.isOpen)
+        out.close()
       cleanUp
     }
     file
   }
 
-  def fetchHTTP(urlString: String) = {
+  private[this] def fetchHTTP(urlString: String) = {
     val connection = new URL(urlString).openConnection.asInstanceOf[HttpURLConnection]
     connection.setRequestMethod("GET")
     connection.addRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)")
     fetch(connection)(urlString, connection.disconnect())
   }
 
-  def fetchFTP(urlString: String) = {
+  private[this] def fetchFTP(urlString: String) = {
     val connection = new URL(urlString).openConnection.asInstanceOf[FtpURLConnection]
     fetch(connection)(urlString)
   }
 
+  def getProtocolConnection(urlString: String) = {
+    urlString.split("://")(0) match {
+      case "http" | "https" => fetchHTTP(urlString)
+      case "ftp" | "sftp" => fetchFTP(urlString)
+      case _ => throw new NoSuchElementException("Unsupported protocol")
+    }
+  }
 
   def futureToFutureTry[T](f: Future[T]): Future[Try[T]] =
     f.map(x => Success(x)).recover { case ex => Failure(ex) }
 
-  val listOfFutureTry: List[Future[Try[File]]] = urls.map(u => futureToFutureTry(Future(fetchHTTP(u))))
+  val listOfFutureTry: List[Future[Try[File]]] = urls.map(u => futureToFutureTry(Future(getProtocolConnection(u))))
   val futureSeq = Future.sequence(listOfFutureTry)
   listOfFutureTry.map(_.collect {
     case Success(x) => s"Successful Download : ${x.getAbsolutePath}"
