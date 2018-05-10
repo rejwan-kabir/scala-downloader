6528e7858e491b56b1317e9ed2dc6c679d726824
Rejwan Shamsul Kabir <rejwan.shuvo@gmail.com>
2018-05-10 22:51:49 +0600

    code done, need to write readme

--- src/main/scala/agoda/Downloader.scala
+++ src/main/scala/agoda/Downloader.scala
@@ -15,25 +15,22 @@ import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success, Try}
 
-// run with command line argument: https://i.pinimg.com/564x/5c/46/e4/shuvo.jpg,ftp://speedtest.tele2.net/512KB.zip,http://longwallpapers.com/Desktop-Wallpaper/rain-wallpapers-hd-For-Desktop-Wallpaper.jpg,ftp://speedtest.tele2.net/2MB.zip,https://wallpaper.wiki/wp-content/uploads/2017/06/Light-water-close-up-nature-rain-wallpapers-HD.jpg,https://i.pinimg.com/564x/2e/88/31/2e8831e90095c14437bbb866dd7cd3ec.jpg,ftp://speedtest.tele2.net/3MB.zip,https://i.pinimg.com/564x/5c/46/e4/5c46e4d74edf8e4c396beda8a126397f.jpg,https://i.pinimg.com/564x/5c/46/e4/imon.jpg,ftp://speedtest.tele2.net/5MB.zip,https://i.pinimg.com/564x/30/f9/51/30f9518869ddedf7bddd5e5a5e65d5a2.jpg,https://i.pinimg.com/564x/3c/64/db/3c64db15ff4a2351cf29634eb7c9240c.jpg,https://i.pinimg.com/564x/70/6c/bd/706cbd9f15223e48168941f89aefff22.jpg,https://i.pinimg.com/564x/5c/46/e4/arshi.jpg /home/rejwan/TEMP/
+// run with command line argument: https://i.pinimg.com/564x/5c/46/e4/shuvo.jpg,ftp://speedtest.tele2.net/512KB.zip,sftp://rejwan:amishuvo@localhost:22/home/rejwan/Dropbox/Photos/IMG_20150825_145004.jpg,http://longwallpapers.com/Desktop-Wallpaper/rain-wallpapers-hd-For-Desktop-Wallpaper.jpg,ftp://speedtest.tele2.net/2MB.zip,https://wallpaper.wiki/wp-content/uploads/2017/06/Light-water-close-up-nature-rain-wallpapers-HD.jpg,sftp://rejwan:amishuvo@localhost:22/home/rejwan/Dropbox/Photos/IMG_20150825_144432.jpg,https://i.pinimg.com/564x/2e/88/31/2e8831e90095c14437bbb866dd7cd3ec.jpg,ftp://speedtest.tele2.net/3MB.zip,https://i.pinimg.com/564x/5c/46/e4/5c46e4d74edf8e4c396beda8a126397f.jpg,https://i.pinimg.com/564x/5c/46/e4/imon.jpg,ftp://speedtest.tele2.net/5MB.zip,https://i.pinimg.com/564x/30/f9/51/30f9518869ddedf7bddd5e5a5e65d5a2.jpg,https://i.pinimg.com/564x/3c/64/db/3c64db15ff4a2351cf29634eb7c9240c.jpg,sftp://rejwan:amishuvo@localhost:22/home/rejwan/Dropbox/Photos/IMG_20150825_145034.jpg,https://i.pinimg.com/564x/70/6c/bd/706cbd9f15223e48168941f89aefff22.jpg,https://i.pinimg.com/564x/5c/46/e4/arshi.jpg
 object Downloader extends App {
   type FilePath = String
   implicit val destination: FilePath = if (args(1).endsWith("/")) args(1) else args + "/"
   val readChunk = 16 * 1024
-  //val urls = args(0).split(',').toList
-  val urls = List(
-    "sftp://rejwan@192.168.104.37:22/home/rejwan/Dropbox/Photos/IMG_20150825_145004.jpg",
-    "sftp://rejwan@192.168.104.37:22/home/rejwan/Dropbox/Photos/IMG_20150825_144432.jpg",
-    "sftp://rejwan@192.168.104.37:22/home/rejwan/Dropbox/Photos/IMG_20150825_145034.jpg"
-  )
+  val urls = args(0).split(',').toList
 
   /*
   val urls = List(
     "https://i.pinimg.com/564x/5c/46/e4/shuvo.jpg",
     "ftp://speedtest.tele2.net/512KB.zip",
+    "sftp://rejwan:amishuvo@localhost:22/home/rejwan/Dropbox/Photos/IMG_20150825_145004.jpg",
     "http://longwallpapers.com/Desktop-Wallpaper/rain-wallpapers-hd-For-Desktop-Wallpaper.jpg",
     "ftp://speedtest.tele2.net/2MB.zip",
     "https://wallpaper.wiki/wp-content/uploads/2017/06/Light-water-close-up-nature-rain-wallpapers-HD.jpg",
+    "sftp://rejwan:amishuvo@localhost:22/home/rejwan/Dropbox/Photos/IMG_20150825_144432.jpg",
     "https://i.pinimg.com/564x/2e/88/31/2e8831e90095c14437bbb866dd7cd3ec.jpg",
     "ftp://speedtest.tele2.net/3MB.zip",
     "https://i.pinimg.com/564x/5c/46/e4/5c46e4d74edf8e4c396beda8a126397f.jpg",
@@ -41,6 +38,7 @@ object Downloader extends App {
     "ftp://speedtest.tele2.net/5MB.zip",
     "https://i.pinimg.com/564x/30/f9/51/30f9518869ddedf7bddd5e5a5e65d5a2.jpg",
     "https://i.pinimg.com/564x/3c/64/db/3c64db15ff4a2351cf29634eb7c9240c.jpg",
+    "sftp://rejwan:amishuvo@localhost:22/home/rejwan/Dropbox/Photos/IMG_20150825_145034.jpg",
     "https://i.pinimg.com/564x/70/6c/bd/706cbd9f15223e48168941f89aefff22.jpg",
     "https://i.pinimg.com/564x/5c/46/e4/arshi.jpg"
   )
@@ -54,7 +52,6 @@ object Downloader extends App {
   }
 
   private[this] def fetch(in: InputStream)(urlString: String, cleanUp: => Unit = ())(implicit destination: FilePath): File = {
-    println("Processing : " + urlString)
     val fileName = urlString.substring(urlString.lastIndexOf('/') + 1)
     val file = new File(destination + fileName)
     val out = FileChannel.open(Paths.get(destination + fileName), CREATE, WRITE)
@@ -70,7 +67,7 @@ object Downloader extends App {
           file.deleteOnExit()
           file.getAbsolutePath
       }
-      splitFileStream.foreach(filePath => {
+      splitFileStream.toList.foreach(filePath => {
         val downloadedFile = FileChannel.open(Paths.get(filePath), READ)
         downloadedFile.transferTo(0, downloadedFile.size, out)
         downloadedFile.close()
@@ -94,25 +91,30 @@ object Downloader extends App {
     val connection = new URL(urlString).openConnection.asInstanceOf[HttpURLConnection]
     connection.setRequestMethod("GET")
     connection.addRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)")
+    connection.setConnectTimeout(30000)
+    connection.setReadTimeout(30000)
     fetch(connection.getInputStream)(urlString, connection.disconnect())
   }
 
   private[this] def fetchFTP(urlString: String) = {
     val connection = new URL(urlString).openConnection.asInstanceOf[FtpURLConnection]
+    connection.setConnectTimeout(30000)
+    connection.setReadTimeout(30000)
     fetch(connection.getInputStream)(urlString)
   }
 
-  // https://kodehelp.com/java-program-for-downloading-file-from-sftp-server/
   private[this] def fetchSFTP(urlString: String) = {
     val uri = new URI(urlString)
     val jsch = new JSch
-    val session = jsch.getSession(uri.getUserInfo, uri.getHost, uri.getPort)
+    val Array(user, password) = uri.getUserInfo.split(":")
+    val session = jsch.getSession(user, uri.getHost, uri.getPort)
     import com.jcraft.jsch.ChannelSftp
     val config = new Properties
     config.put("StrictHostKeyChecking", "no")
     session.setConfig(config)
-    session.setPassword("tigerit")
+    session.setPassword(password)
     session.connect()
+    session.setTimeout(30000)
     val channel = session.openChannel("sftp")
     channel.connect()
     val channelSftp = channel.asInstanceOf[ChannelSftp]
@@ -125,6 +127,7 @@ object Downloader extends App {
   }
 
   def getProtocolConnection(urlString: String) = {
+    println("Processing : " + urlString)
     urlString.split("://")(0) match {
       case "http" | "https" => fetchHTTP(urlString)
       case "ftp" | "ftps" => fetchFTP(urlString)
